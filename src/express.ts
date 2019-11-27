32f4e503760e3c17179a1da26a1fb1d1871bfc9d
Пинчук Виталий А <Pinchuk_VA@pac.local>
2019-11-27 19:15:18 +0300

    add express and esm resolving wrong (on transpiling code of imports we have not .js extension by default)

---
+++ src/express.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import path from 'path';
+
+const port = 3000;
+const app = express();
+
+app.use(express.static(
+	path.resolve('dist'),
+	{ cacheControl: false },
+));
+
+app.get('/', (req, res): void => {
+	res.sendFile(
+		path.resolve('dist', 'index.html'),
+	);
+});
+
+// TODO: resolve to .js extensions instead of replacing imports in js-files
+
+const server = app.listen(port, (): void => {
+	console.log(`Example app listening on port ${port}!`);
+});
+
+// @link: https://flaviocopes.com/node-terminate-program/
+process.on('SIGTERM', (): void => {
+	server.close((): void => {
+		console.log('Process terminated');
+	});
+});
