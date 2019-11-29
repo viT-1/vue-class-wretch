25843f4a19c52c51b30512876e0372bb11c36c93
Пинчук Виталий А <Pinchuk_VA@pac.local>
2019-11-29 12:39:55 +0300

    test wretch in VueClassComponent mounted method

--- replace.config.js
+++ replace.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-	files: ['dist/*.js', 'dist/*.*.js'],
+	files: ['dist/*.js', 'dist/*.esm.js'],
 	from: [
 		/\'vue-class-component\'/g,
 		/\'vue\'/g,
-		/process\.env\.NODE_ENV/g
+		/process\.env\.NODE_ENV/g,
+		/\'wretch\'/g
 	],
 	to: [
 		'\'./vue-class-component.esm.js\'',
 		'\'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js\'',
-		'JSON.stringify("production")'
+		'JSON.stringify("production")',
+		'\'https://cdn.jsdelivr.net/npm/wretch@1.6.0/dist/bundle/wretch.esm.js\''
 	],
 };
--- src/SomeComponent.ts
+++ src/SomeComponent.ts
@@ -1,9 +1,22 @@
+// import Vue from 'vue';
 import Component from 'vue-class-component';
+import wretch from 'wretch';
 
 import { BaseComponent } from './BaseComponent';
 
 @Component({
 	name: 'my-component',
-	template: '<span class="my-component">Vue Started!</span>',
+	template: '<div class="my-component"><h3>Vue Started!</h3><h4>tsconfig.json:</h4><p>{{tsconfig}}</p></div>',
 })
-export class SomeComponent extends BaseComponent {}
+export class SomeComponent extends BaseComponent {
+	// we cannot doing this in constructor due to class properties reactivity
+	mounted() {
+		wretch('/tsconfig.json')
+			.get()
+			.json(jsonResp => {
+				this.tsconfig = jsonResp as String;
+			});
+	}
+
+	tsconfig: String | null = null;
+}
